e3260cacc559016f2c8d139344e18d42f95258c8
zuGROB <68923431+zuGROB@users.noreply.github.com>
2024-07-17 17:10:13 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -21,7 +21,15 @@ suspicious_activity = defaultdict(list)
 
 # Список паттернов подозрительных запросов
 SUSPICIOUS_PATTERNS = [
-'nope, not sharing in any case~'
+    r'phpMyAdmin',
+    r'/cgi-bin/',
+    r'setup.cgi',
+    r'cmd=rm+-rf',
+    r'wget+http://',
+    r'\$\(.*\)',  # Попытки выполнения команд
+    r'eval\(',    # Попытки выполнения кода
+    r'base64_decode\(',
+    r'(?:\.\.\/){2,}',  # Path traversal
 ]
 
 # Функции для работы с файлами
